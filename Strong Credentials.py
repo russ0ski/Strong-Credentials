3d1c8522ea41a2b907c735f8ecd430192640af8f
11-h-30 <juancarloslevjim@gmail.com>
2024-06-13 01:47:03 +0200

    Actualización final de Strong Credentials.py

--- Strong Credentials.py
+++ Strong Credentials.py
@@ -1,4 +1,4 @@
-# Importamos la librería Random que nos ayudará con la aleatoriedad de las Contraseñas
+# Importamos la librería Random que nos ayudará con la aleatoriedad de las contraseñas
 import random
 
 # Definimos las 3 variables donde almacenaremos los caracteres alfanuméricos y los símbolos
@@ -7,14 +7,14 @@ numeros = "123456789"
 simbolos = "#@=<>{[]}?¿-;}][]}"
 
 # Definimos Unión, donde combinaremos las variables anteriores
-# Definimos Longitud, donde especificaremos el largo de las Contraseñas
+# Definimos Longitud, donde especificaremos el largo de las contraseñas
 union = f'{letras}{numeros}{simbolos}'
 longitud = 20
 
-# Definimos Contraseña, donde la función random actuará sobre las variables Union y Longitud
+# Definimos Contraseña, donde la función random actuará sobre las variables Unión y Longitud
 # Definimos Contraseña_final, donde se almacenará en una cadena vacía el resultado de Contraseña
 contraseña = random.sample(union, longitud)
 contraseña_final = "".join(contraseña)
 
-# Imprimos por pantalla la Contraseña resultante junto a un mensaje descriptivo
+# Imprimimos por pantalla la contraseña resultante junto a un mensaje descriptivo
 print("-> Tu Contraseña generada es:", contraseña_final)
